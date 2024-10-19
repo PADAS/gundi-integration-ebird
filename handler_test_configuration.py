e3a27caf8634929e66c16e0488f1e275d1e4f374
Chris Doehring <chrisdoehring@gmail.com>
2024-10-19 10:15:57 -0700

    prune handler test script.

--- handler_test_configuration.py
+++ handler_test_configuration.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
                     type="auth",
                     name="Auth",
                     value="auth",
-                    description="Chrisdo Gfw Auth action",
+                    description="",
                     action_schema={
                         "type": "object",
                         "title": "AuthenticateConfig",
@@ -67,7 +67,7 @@ if __name__ == "__main__":
             ],
             webhook=None,
         ),
-        base_url="https://ebird.tempuri.org",
+        base_url="https://api.ebird.org",
         enabled=True,
         owner=Organization(
             id=UUID("b56b585d-7f94-4a45-b8af-bb7dc6a9c731"),
@@ -77,7 +77,7 @@ if __name__ == "__main__":
         configurations=[
             IntegrationActionConfiguration(
                 id=UUID("7c0cbf42-ad62-4725-8380-e4cf29acc406"),
-                integration=UUID("5ac83294-bda3-43c8-b58e-566024b03543"),
+                integration=UUID("e9c1eef0-7c28-46bb-8155-fe9b31dedce7"),
                 action=IntegrationActionSummary(
                     id=UUID("1306da74-7e87-45a0-a5de-c11974e4e63e"),
                     type="pull",
@@ -90,7 +90,7 @@ if __name__ == "__main__":
             ),
             IntegrationActionConfiguration(
                 id=UUID("91930701-0cf3-4201-a4a5-02b458c460e1"),
-                integration=UUID("5ac83294-bda3-43c8-b58e-566024b03543"),
+                integration=UUID("e9c1eef0-7c28-46bb-8155-fe9b31dedce7"),
                 action=IntegrationActionSummary(
                     id=UUID("e0d2b2de-a277-4f67-89ef-13ef0e07623d"),
                     type="auth",
@@ -103,18 +103,5 @@ if __name__ == "__main__":
                 },
             ),
         ],
-        webhook_configuration=None,
-        default_route=ConnectionRoute(
-            id=UUID("4a98b8bb-1be7-479a-8814-e1c9843159e3"),
-            name="Test eBird Connection - Default Route",
-        ),
-        additional={},
-        status={
-            "id": "mockid-b16a-4dbd-ad32-197c58aeef59",
-            "is_healthy": True,
-            "details": "Last observation has been delivered with success.",
-            "observation_delivered_24hrs": 50231,
-            "last_observation_delivered_at": "2023-03-31T11:20:00+0200",
-        },
     )
-    asyncio.run(action_pull_events(integration, action_config))
+    asyncio.run(action_pull_events(integration=integration, action_config=action_config))
